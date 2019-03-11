0694941f0ca5f2becf4bb8521d48d4c228b1beec
Colin Lee <frontend1991@163.com>
2019-03-11 16:52:47 +0800

    complete form validate.

--- src/router.js
+++ src/router.js
@@ -18,6 +18,14 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/form',
+      name: 'form',
+      // route level code-splitting
+      // this generates a separate chunk (form.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "form" */ './views/Form.vue')
     }
   ]
 })
